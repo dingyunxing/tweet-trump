c78d83702c9f1129fcef36ff521114bca803c7be
Ding Yunxing <dingyunxing@Dings-MacBook-Pro.local>
2018-03-09 22:17:52 +1300

    grab data

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @user
+
+# Consume:
+CONSUMER_KEY    = 'xIoep3YBS3ikt4K6SSqKEFZAp'
+CONSUMER_SECRET = 'lFQrRC4BIoY3PjfdpTMDUIajy7kymnitxUz9SVDDbxAmX6KYHr'
+
+# Access:
+ACCESS_TOKEN  = '4694750125-ob0kSakeEsi0sMrXwG3fg0bwmowoNFsINTDzyc0'
+ACCESS_SECRET = 'OsXvxQOjh2nesusg0XZOFtDNCitQSjid2TrGMfwX3UhZC'
